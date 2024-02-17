a2bc88bd350e64912803ad4e7f4a5f22e19cb41e
BaronessT03 <josephine.davina.thompson@gmail.com>
2024-02-17 13:20:51 -0500

    Scores updated

--- Scenes/Score Nodes.gd
+++ Scenes/Score Nodes.gd
@@ -11,5 +11,28 @@ func _process(delta):
 	pass
 
 
-func _on_area_2d_area_entered(area):
-	pass # Replace with function body.
+
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("woodDisk") :
+		Global.woodPoints = 5 + Global.woodPoints
+	else :
+		if body.is_in_group("blackDisk") :
+			Global.blackPoints = 5 + Global.blackPoints
+
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("woodDisk") :
+		Global.woodPoints =  Global.woodPoints - 5
+	else :
+		if body.is_in_group("blackDisk") :
+			Global.blackPoints = Global.blackPoints - 5
+
+
+func _on__point_body_entered(body):
+	if body.is_in_group("woodDisk") :
+		Global.woodPoints =  Global.woodPoints +20
+	else :
+		if body.is_in_group("blackDisk") :
+			Global.blackPoints = Global.blackPoints +20
