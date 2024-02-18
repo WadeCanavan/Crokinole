aa7b710428360a89dfad1cbc879f07770928b02d
Noki <noki.heroux@gmail.com>
2024-02-17 23:20:43 -0500

    shotCount hotfix
    small fix for shotCount

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -3,7 +3,7 @@ var lastValue = 0
 
 var woodCount = 0
 var blackCount = 0
-var shotCount
+var shotCount = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var disc = $Discs/disc
@@ -34,7 +34,7 @@ func on_disc_stop():
 	get_node("/root/Main/scoreBox/Score").text = str("  Player 1: ", Global.woodPoints, "\n  Player 2: ", Global.blackPoints)
 	shotCount = shotCount + 1
 	print(shotCount)
-	if counter == 11:
+	if shotCount == 11:
 		get_tree().change_scene_to_file("res://Scenes/winner.tscn")
 	var discInst = load("res://Scenes/disc.tscn").instantiate()
 	#discInst.get_parent().remove_child(discInst)
