f051e92e91e55a961f64551ad694866ee083d5b8
Noki <noki.heroux@gmail.com>
2024-02-17 23:36:52 -0500

    rahhhhhhh

--- Scenes/Score Nodes.gd
+++ Scenes/Score Nodes.gd
@@ -3,7 +3,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -38,14 +39,14 @@ func _on_area_2d_body_exited(body):
 
 
 func _on__point_body_entered(body):
-	if body.is_in_group("woodDisk") and body.linear_velocity.length() <= 75:
+	if body.is_in_group("woodDisk") and body.linear_velocity.length() <= 100:
 		Global.woodPoints =  Global.woodPoints +20
 		$"20 point/RichTextLabel".visible = true
 		print(Global.woodPoints)
 		
 		body.queue_free()
 	else :
-		if body.is_in_group("blackDisk") and body.linear_velocity.length() <= 75:
+		if body.is_in_group("blackDisk") and body.linear_velocity.length() <= 100:
 			
 			Global.blackPoints = Global.blackPoints +20
 			$"20 point/RichTextLabel".visible = true
