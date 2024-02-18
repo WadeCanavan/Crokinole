087330a4252594aa79b167b772484de5a821d944
BaronessT03 <josephine.davina.thompson@gmail.com>
2024-02-17 19:21:05 -0500

    Points work!!

--- Scenes/Score Nodes.gd
+++ Scenes/Score Nodes.gd
@@ -17,6 +17,7 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("woodDisk") :
 		Global.woodPoints = 5 + Global.woodPoints
+		print(Global.woodPoints)
 	else :
 		if body.is_in_group("blackDisk") :
 			Global.blackPoints = 5 + Global.blackPoints
@@ -25,28 +26,34 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("woodDisk") :
 		Global.woodPoints =  Global.woodPoints - 5
+		print(Global.woodPoints)
 	else :
 		if body.is_in_group("blackDisk") :
 			Global.blackPoints = Global.blackPoints - 5
 
 
 func _on__point_body_entered(body):
-	if body.is_in_group("woodDisk") and body.linear_velocity == 0:
+	if body.is_in_group("woodDisk") and body.linear_velocity.length() <= 10:
 		Global.woodPoints =  Global.woodPoints +20
-		queue_free()
+		print(Global.woodPoints)
+		
+		body.queue_free()
 	else :
-		if body.is_in_group("blackDisk") and body.linear_velocity == 0:
+		if body.is_in_group("blackDisk") and body.linear_velocity.length() <= 10:
 			
 			Global.blackPoints = Global.blackPoints +20
-			queue_free()
+			body.queue_free()
 
 
 func _on_play_area_body_exited(body):
+	
 	if body.is_in_group("woodDisk") :
-		
-		queue_free()
+		Global.woodPoints =  Global.woodPoints - 5
+		print(Global.woodPoints)
+		body.queue_free()
 	else :
 		if body.is_in_group("blackDisk") :
 			
+			Global.blackPoints = Global.blackPoints - 5
 			
-			queue_free()
+			body.queue_free()
