f3fcb380445e6dd7db8f45bef3a08337ff76af31
Wade Canavan <112517708+WadeCanavan@users.noreply.github.com>
2024-02-17 21:17:23 -0500

    disc can only be flicked once

--- Scripts/disc.gd
+++ Scripts/disc.gd
@@ -5,15 +5,16 @@ var clickPos
 
 var lastValue = 0 
 var curValue = 0 
+var shot = false
 
 func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and  event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+	if event is InputEventMouseButton and  event.button_index == MOUSE_BUTTON_LEFT and event.pressed and not shot:
 			#print("Left button was clicked at ", event.position)
 			clickPos = event.position
 			pressed = true
 			
 func _input(event):
-	if event is InputEventMouseButton and  event.button_index == MOUSE_BUTTON_LEFT and not event.pressed and pressed:
+	if event is InputEventMouseButton and  event.button_index == MOUSE_BUTTON_LEFT and not event.pressed and pressed and not shot:
 			pressed = false
 			#print("Left button was released at ", event.position)
 			var force = Vector2(clickPos.x - event.position.x, clickPos.y - event.position.y)
@@ -21,17 +22,19 @@ func _input(event):
 			#print("distance to disc ", distance)
 			#print(clickPos.x, event.position.x, clickPos.y, event.position.y)
 			linear_velocity = force * 5
+			shot = true
 
 func _integrate_forces(state):
 	#print(curValue)
-	if curValue != 0 :
+	if curValue != 0 and not shot:
 		var origin = Vector2( 576, 316.039 )
 		#print( origin.distance_to(Vector2(position.x,position.y)))
 		var angle = origin.angle_to_point( Vector2(position.x,position.y))
 		position.x = origin.x + cos(angle + (curValue)) * 246.105651855469 
 		position.y = origin.y + sin(angle + (curValue)) * 246.105651855469 
 		curValue = 0
-		
+
+
 func _on_h_slider_value_changed(value):
 	if lastValue < value:
 		curValue = value/200
