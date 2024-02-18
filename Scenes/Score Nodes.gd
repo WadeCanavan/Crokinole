cf7df2eb7854139ca38d165e1c16ccf50e0d19ed
BaronessT03 <josephine.davina.thompson@gmail.com>
2024-02-17 23:25:57 -0500

    HEader for getting a 20!

--- Scenes/Score Nodes.gd
+++ Scenes/Score Nodes.gd
@@ -16,27 +16,31 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("woodDisk") :
+		$"20 point/RichTextLabel".visible = false
 		Global.woodPoints = 5 + Global.woodPoints
 		print(Global.woodPoints)
 	else :
 		if body.is_in_group("blackDisk") :
+			$"20 point/RichTextLabel".visible = false
 			Global.blackPoints = 5 + Global.blackPoints
 
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("woodDisk") :
+		$"20 point/RichTextLabel".visible = false
 		Global.woodPoints =  Global.woodPoints - 5
 		print(Global.woodPoints)
 		
 	else :
 		if body.is_in_group("blackDisk") :
+			$"20 point/RichTextLabel".visible = false
 			Global.blackPoints = Global.blackPoints - 5
 
 
 func _on__point_body_entered(body):
 	if body.is_in_group("woodDisk") and body.linear_velocity.length() <= 75:
 		Global.woodPoints =  Global.woodPoints +20
-		
+		$"20 point/RichTextLabel".visible = true
 		print(Global.woodPoints)
 		
 		body.queue_free()
@@ -44,6 +48,7 @@ func _on__point_body_entered(body):
 		if body.is_in_group("blackDisk") and body.linear_velocity.length() <= 75:
 			
 			Global.blackPoints = Global.blackPoints +20
+			$"20 point/RichTextLabel".visible = true
 			body.queue_free()
 
 
@@ -51,6 +56,7 @@ func _on_play_area_body_exited(body):
 	
 	if body.is_in_group("woodDisk") :
 		Global.woodPoints =  Global.woodPoints - 5
+		$"20 point/RichTextLabel".visible = false
 		
 		print(Global.woodPoints)
 		body.queue_free()
@@ -58,5 +64,6 @@ func _on_play_area_body_exited(body):
 		if body.is_in_group("blackDisk") :
 			
 			Global.blackPoints = Global.blackPoints - 5
+			$"20 point/RichTextLabel".visible = false
 			
 			body.queue_free()
