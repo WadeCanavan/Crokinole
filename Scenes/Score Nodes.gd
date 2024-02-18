8801f00dfcf024ee6d120d2e3e692371b489b504
Wade Canavan <112517708+WadeCanavan@users.noreply.github.com>
2024-02-18 03:30:10 -0500

    game is gaming

--- Scenes/Score Nodes.gd
+++ Scenes/Score Nodes.gd
@@ -1,6 +1,5 @@
 extends Node
 
-signal twenty
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
@@ -10,10 +9,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-
-
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("wood") :
@@ -39,6 +34,7 @@ func _on_area_2d_body_exited(body):
 
 
 func _on__point_body_entered(body):
+	var name = body.get_instance_id()
 	var origin = Vector2( 576, 316.039 )
 	if body.is_in_group("wood") and body.linear_velocity.length() <= 200:
 		Global.woodPoints =  Global.woodPoints +20
@@ -49,9 +45,8 @@ func _on__point_body_entered(body):
 		await get_tree().create_timer(4.0).timeout
 		$"20 point/RichTextLabel".visible = false
 		$"20 point/TextureRect".visible = false
-		
-		print(Global.woodPoints)
-		
+
+		#print(Global.woodPoints)
 		
 	else :
 		if body.is_in_group("black") and body.linear_velocity.length() <= 200:
@@ -64,20 +59,21 @@ func _on__point_body_entered(body):
 			await get_tree().create_timer(4.0).timeout
 			$"20 point/RichTextLabel".visible = false
 			$"20 point/TextureRect2".visible = false
-			twenty.emit()
+
 
 func _on_play_area_body_exited(body):
-	
+	var name = body.get_instance_id()
 	if body.is_in_group("wood") :
 		Global.woodPoints =  Global.woodPoints - 5
 		#$"20 point/RichTextLabel".visible = false
 		
 		print(Global.woodPoints)
 		body.queue_free()
+
 	else :
 		if body.is_in_group("black") :
 			
 			Global.blackPoints = Global.blackPoints - 5
 			#$"20 point/RichTextLabel".visible = false
-			
 			body.queue_free()
+
--- Scenes/main.gd
+++ Scenes/main.gd
@@ -5,15 +5,16 @@ var woodCount = 0
 var blackCount = 0
 var shotCount = 0
 var curDisc
+var lastDisc
 signal slider
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	new_disc()
 	curDisc.discShot.connect(on_disc_shot)
 	curDisc.endShot.connect(on_disc_stop)
-	$"Score Nodes".twenty.connect(new_disc) #triggers when we score 20
 	$"Score Nodes/20 point/RichTextLabel".visible = false
 	$"Score Nodes/20 point/TextureRect".visible = false
+	$"Score Nodes/20 point/TextureRect2".visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -34,23 +35,27 @@ func _boardCheck():
 	pass
 	
 func on_disc_shot():
-	print("signaled")
+	var nowDisc = curDisc
+	await get_tree().create_timer(5.0).timeout
+	if nowDisc == curDisc:
+		new_disc()
 
 func on_disc_stop():
-	get_node("/root/Main/scoreBox/Score").text = str("  Player 1: ", Global.woodPoints, "\n  Player 2: ", Global.blackPoints)
-	print(shotCount)
-	if shotCount == 12:
-		get_tree().change_scene_to_file("res://Scenes/winner.tscn")
 	new_disc()
-	curDisc.discShot.connect(on_disc_shot)
-	curDisc.endShot.connect(on_disc_stop)
 
 func new_disc():
-	print("new disc")
+	if (shotCount % 2):
+		get_node("/root/Main/scoreBox/Score").text = str("  Player 1: ", Global.woodPoints, "\n[color=blue]  Player 2: ", Global.blackPoints ,"[/color]")
+	else:
+		get_node("/root/Main/scoreBox/Score").text = str("[color=blue]  Player 1: ", Global.woodPoints, "[/color]\n  Player 2: ", Global.blackPoints)
+	if shotCount >= 12:
+		get_tree().change_scene_to_file("res://Scenes/winner.tscn")
 	shotCount = shotCount + 1
 	curDisc = load("res://Scenes/disc.tscn").instantiate()
+	print("new disc ", curDisc)
 	#discInst.get_parent().remove_child(discInst)
 	$Discs.add_child(curDisc)
+	print("shots ", shotCount, "   ", shotCount % 2 )
 	if (shotCount % 2):
 		curDisc.position = Vector2(768, 472)
 		curDisc.add_to_group("wood")
@@ -58,6 +63,11 @@ func new_disc():
 		curDisc.position = Vector2(384, 160.078)
 		curDisc.add_to_group("black")
 		curDisc.get_child(0,false).texture = load("res://Assets/materials/black.tres")
+	curDisc.discShot.connect(on_disc_shot)
+	curDisc.endShot.connect(on_disc_stop)
 
 func _on_h_slider_value_changed(value):
 	slider.emit(value)
+
+func _on_exit_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Title.tscn")
